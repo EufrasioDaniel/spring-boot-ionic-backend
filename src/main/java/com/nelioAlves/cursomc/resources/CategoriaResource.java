fb9cd663fa03d3841433bda39e28f153f83b7b01
Eufrasio Daniel <edaniel@indracompany.com>
2022-06-17 12:45:01 -0300

    inserindo uma nova Categoria com post

--- src/main/java/com/nelioAlves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioAlves/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package com.nelioAlves.cursomc.resources;
 
+import java.net.URI;
+
+import javax.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioAlves.cursomc.domain.Categoria;
 import com.nelioAlves.cursomc.services.CategoriaService;
@@ -20,5 +26,13 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) { //Reponseentity é um objeto que tem varias informações de resposta http
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) //Responde pelo get no end point categorias
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
