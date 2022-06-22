f38c65d9923321386295e003c2368d9d9fb84edc
Eufrasio Daniel <edaniel@indracompany.com>
2022-06-22 12:27:29 -0300

    email HTML

--- src/main/java/com/nelioAlves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioAlves/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.nelioAlves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.nelioAlves.cursomc.domain.Pedido;
@@ -7,4 +9,7 @@ import com.nelioAlves.cursomc.domain.Pedido;
 public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
